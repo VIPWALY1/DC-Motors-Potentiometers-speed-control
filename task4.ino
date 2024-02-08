e012dfb545ed75a0bd0b0d4be14e67aa2c492a4b
VIPWALY1 <157975475+VIPWALY1@users.noreply.github.com>
2024-02-08 18:58:36 +0200

    Add files via upload

--- task4.ino
+++ task4.ino
@@ -3,8 +3,8 @@ int puz2 = 6;
 int re = A0;
 int re2 = A1;
 int reading=0;
-int sw = 13;
-int sw2 = 12;
+int sw = 9;
+int sw2 = 10;
 char reading3;
 int reading2 = 0;
 
@@ -18,13 +18,12 @@ void setup()
   pinMode(sw2, INPUT); 
   Serial.begin(9600);}
 void loop(){  reading2=analogRead(re2); reading=analogRead(re);
-  if (Serial.available()) {
-  if(reading3=='f'){reading2=analogRead(re2); analogWrite(puz2,reading2);}
-  if(reading3=='s'){reading =analogRead(re ); analogWrite(puz ,reading );}  
+  if (Serial.available()) {char reading3=Serial.read();
+  if(reading3=='f'){  analogWrite(puz2,!reading2);}
+  if(reading3=='s'){  analogWrite(puz ,!reading );}  
   }
   if (digitalRead(sw) == 1) { 
-  reading3=Serial.read();
-   analogWrite(puz,reading / 4);}
+    analogWrite(puz,reading / 4);}
   else{ analogWrite(puz,0);}
   
   
